2921edb186d447e31370cd9b779b67c4bf1ac9e6
Arne <arneflatekval@hotmail.com>
2018-11-16 08:55:40 +0100

    Cleaned up TempratureSensor.java

--- IoT/Oblig2/src/no/hvl/dat159/roomcontrol/TemperatureSensor.java
+++ IoT/Oblig2/src/no/hvl/dat159/roomcontrol/TemperatureSensor.java
@@ -1,18 +1,12 @@
 package no.hvl.dat159.roomcontrol;
 
-public class TemperatureSensor implements Runnable{
+public class TemperatureSensor{
 
 	private Room room;
 
 	public TemperatureSensor(Room room) {
 
 		this.room = room;
-	}
-
-	@Override
-	public void run() {
-		//MQTTPubTemperature mqtt = new MQTTPubTemperature(this);
-		//mqtt.run();
 	}
 
 	public double read() {
